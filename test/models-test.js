39ebc18b61a2c5682bddb6f27f8dfeadba68f427
Dumitru Cantea <canteadumitru@gmail.com>
2015-10-27 12:49:58 +0200

    more secure entity name 'uniqueName'
    remove any non-letter and non-digit

--- test/models-test.js
+++ test/models-test.js
@@ -79,6 +79,17 @@ describe('models', function() {
 			assert.equal(undefined, model.getData().entityId);
 			assert.equal('me', model.getData().name);
 		});
+
+		it('#createUniqueName', function() {
+			var name = 'Team-A F.C.';
+			assert.equal('team a fc', EntityName.createUniqueName(name));
+
+			name = 'T.Lastname';
+			assert.equal('t lastname', EntityName.createUniqueName(name));
+
+			name = 'Düsseldorf 29, Köln, Москва, 北京市, إسرائيل !@#$ «Барсело́на» :"șțÂĂîăâăă';
+			assert.equal('dusseldorf 29 koln москва 北京市 إسرائيل барселона staaiaaaa', EntityName.createUniqueName(name));
+		});
 	});
 
 	describe('Entity', function() {
@@ -96,6 +107,17 @@ describe('models', function() {
 			assert.equal('Entity', model.config.name);
 			assert.equal(undefined, model.getData().entityId);
 			assert.equal('me', model.getData().name);
+		});
+
+		it('#createSlug', function() {
+			var name = 'Team F.C.';
+			assert.equal('team-fc', Entity.createSlug(name));
+
+			name = 'Düsseldorf 29, Köln, Москва, 北京市, إسرائيل !@#$ «Барсело́на» Влади́мир Влади́мирович Пу́тин ☢☠☤☣☭☯☮☏☔☎☀★☂☃✈✉';
+			assert.equal('dusseldorf-29-koln-moskva-barselona-vladimir-vladimirovich-putin', Entity.createSlug(name));
+
+			name = 'Жанна Фриске';
+			assert.equal('zhanna-friske', Entity.createSlug(name));
 		});
 	});
 });
